ae27f0b8432becb10646fe233aa65628c1f311a9
Hoe Hao Cheng <haochengho12907@gmail.com>
2020-09-20 15:49:43 +0800

    Implement as_ne_bytes for floats

--- library/core/src/num/f64.rs
+++ library/core/src/num/f64.rs
@@ -787,6 +787,35 @@ impl f64 {
         self.to_bits().to_ne_bytes()
     }
 
+    /// Return the memory representation of this floating point number as a byte array in
+    /// native byte order.
+    ///
+    /// [`to_ne_bytes`] should be preferred over this whenever possible.
+    ///
+    /// [`to_ne_bytes`]: #method.to_ne_bytes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let num = 12.5f64;
+    /// let bytes = num.as_ne_bytes();
+    /// assert_eq!(
+    ///     bytes,
+    ///     if cfg!(target_endian = "big") {
+    ///         &[0x40, 0x29, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    ///     } else {
+    ///         &[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x29, 0x40]
+    ///     }
+    /// );
+    /// ```
+    #[unstable(feature = "float_as_bytes", issue = "64464")]
+    #[inline]
+    pub fn as_ne_bytes(&self) -> &[u8; 8] {
+        // SAFETY: `u64` is a plain old datatype so we can always transmute to it
+        let bits = unsafe { mem::transmute::<&f64, &u64>(self) };
+        bits.as_ne_bytes()
+    }
+
     /// Create a floating point value from its representation as a byte array in big endian.
     ///
     /// # Examples
